7d7b6c5551feb09f00265111de3ca616ad2522d1
arthur85c <arthur85c@gmail.com>
2018-07-30 14:49:09 -0400

    Update

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -179,7 +179,7 @@ const deleteSuccess = function (response) {
 
 const updateSuccess = function (response) {
   $(`.modal-backdrop`).remove()
-  $('body').removeClass(modal-open)
+  $('body').removeClass('modal-open')
   api.findProducts()
     .then(showProducts)
 }
